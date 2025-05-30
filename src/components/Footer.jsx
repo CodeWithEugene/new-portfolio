b3a1acaaa4e6bc9c136d9da2fc1acd8d67d20e22
Eugene Gabriel <103780583+CodeWithEugene@users.noreply.github.com>
2025-05-30 09:55:36 +0300

    Set up React portfolio project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p className="copyright">&copy; Proudly Made by <a href="https://eugeniuss.netlify.app/">Eugene Gabriel</a>.</p>
+    </footer>
+  );
+};
+
+export default Footer;
